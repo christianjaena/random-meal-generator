3c2ba9c468744ba633d7038e6a9be322ad6a9f4a
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-07-26 20:50:14 +0800

    Updates

---
+++ precache-manifest.3ced1d39b7d9c8b70fa8d6000d1f2ba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232c615142c6eb12e1258b4fcf453bb1",
+    "url": "/react-random-meal-generator/index.html"
+  },
+  {
+    "revision": "a4a03fe39e9fbddaec50",
+    "url": "/react-random-meal-generator/static/js/2.a7caec6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-random-meal-generator/static/js/2.a7caec6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92531917aa597c93e03f",
+    "url": "/react-random-meal-generator/static/js/main.0556e5b7.chunk.js"
+  },
+  {
+    "revision": "f876a5e3b0b88a40e4c9",
+    "url": "/react-random-meal-generator/static/js/runtime-main.acd11326.js"
+  },
+  {
+    "revision": "6183e7e4ca884514174d77975d0e61b0",
+    "url": "/react-random-meal-generator/static/media/moroccan-flower.6183e7e4.png"
+  }
+]);
